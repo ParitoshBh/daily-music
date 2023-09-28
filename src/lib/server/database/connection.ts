32fb258b459efe0d948e3b0a73219eced969b9c5
Paritosh Bhatia <mail@paritoshbh.me>
2023-09-28 15:09:52 +0000

    Remove config library in favour of env variable

--- src/lib/server/database/connection.ts
+++ src/lib/server/database/connection.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-import config from 'config';
+// import config from 'config';
 import type { Database } from "./schema";
-import type { DatabaseConfig } from "../../../types/config";
+// import type { DatabaseConfig } from "../../../types/config";
 
-const dbConfig = config.get<DatabaseConfig>('database');
+// const dbConfig = config.get<DatabaseConfig>('database');
 
-export const supabase = createClient<Database>(dbConfig.supabaseUrl, dbConfig.supabaseKey);
+// export const supabase = createClient<Database>(dbConfig.supabaseUrl, dbConfig.supabaseKey);
+export const supabase = createClient<Database>(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
