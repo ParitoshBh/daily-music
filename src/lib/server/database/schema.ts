d786c074ec15629d0b44e26796d65b3bd218a219
ParitoshBh <mail@paritoshbh.me>
2023-10-16 02:50:37 +0000

    Update db schema

--- src/lib/server/database/schema.ts
+++ src/lib/server/database/schema.ts
@@ -1,36 +1,76 @@
-export type Json = string | number | boolean | null | { [key: string]: Json } | Json[]
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
-      suggestions: {
+      forms: {
         Row: {
+          access_id: string | null
+          created_at: string | null
           id: number
-          user_id: string
           name: string
-          band: string
-          title: string
-          url: string
-          inserted_at: string
+          user_id: string | null
         }
         Insert: {
-          id?: number
-          user_id: string
           name: string
-          band: string
-          title: string
-          url: string
-          inserted_at?: string
+          user_id?: string | null
         }
         Update: {
+          access_id?: string | null
+          created_at?: string | null
           id?: number
-          user_id?: string
           name?: string
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "forms_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      suggestions: {
+        Row: {
+          band: string
+          form_id: number
+          id: number
+          inserted_at: string
+          name: string
+          title: string
+          url: string
+        }
+        Insert: {
+          band: string
+          form_id: number
+          name: string
+          title: string
+          url: string
+        }
+        Update: {
           band?: string
+          form_id?: number
+          id?: number
+          inserted_at?: string
+          name?: string
           title?: string
           url?: string
-          inserted_at?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "suggestions_form_id_fkey"
+            columns: ["form_id"]
+            referencedRelation: "forms"
+            referencedColumns: ["id"]
+          }
+        ]
       }
     }
     Views: {
@@ -40,6 +80,9 @@ export interface Database {
       [_ in never]: never
     }
     Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
       [_ in never]: never
     }
   }
