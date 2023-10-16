e22fc9cf50e9792b9d5fd1034355a4b145d12380
ParitoshBh <mail@paritoshbh.me>
2023-10-16 02:51:26 +0000

    Add functions to create and validate form id

--- src/lib/server/services/formService.ts
+++ src/lib/server/services/formService.ts
@@ -1,3 +1,6 @@
+import { InvalidFormException } from "../../../exceptions/form";
+import { FormCreationFailedException } from "../../../exceptions/form";
+import { supabase } from "../database/connection";
 import { formSchema } from "../schemas/formSchema";
 
 type ValidatedFormData = {
@@ -7,6 +10,11 @@ type ValidatedFormData = {
     url: string;
 };
 
+type NewForm = {
+	id: string;
+	url: string;
+}
+
 export function validateForm(data: FormData): ValidatedFormData {
 	return formSchema.parse({
 		name: data.get('name'),
@@ -14,4 +22,36 @@ export function validateForm(data: FormData): ValidatedFormData {
 		title: data.get('title'),
 		url: data.get('url')
 	});
+}
+
+export async function create(name: string, userId: string): Promise<NewForm> {
+	const { data } = await supabase.from('forms')
+		.insert({
+			name: name,
+			user_id: userId,
+		})
+		.select()
+		.throwOnError();
+	const formId = data?.pop()?.access_id;
+	if (!formId) {
+		throw new FormCreationFailedException('Failed to retrieve access_id');
+	}
+	
+	return {
+		id: formId,
+		url: `https://daily-music.vercel.app/form/${formId}`
+	};
+}
+
+export async function validateAccessId(accessId?: string) {
+	if (accessId) {
+		const { data, error } = await supabase.from('forms')
+        	.select()
+        	.eq('access_id', accessId);
+		if (data?.length === 0 || error) {
+			throw new InvalidFormException('Invalid form');
+		}
+		return data[0];		
+	}
+	throw new InvalidFormException('Form access id is invalid');
 }
